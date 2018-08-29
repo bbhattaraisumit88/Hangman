618684a5bef4776502d1a06635b3cd4fe8cc1715
bsumitiw <bsumitiw@gmail.com>
2018-08-29 15:39:59 +0545

    Added POCO Entities And DTO For User Authentication

---
+++ Hangman.Domain/DTO/User/RegisrationDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hangman.Domain
+{
+    public class RegistrationDTO
+    {
+        [Required(ErrorMessage = "UserName cannot be empty")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email cannot be empty")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password cannot be empty")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "FirstName cannot be empty")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName cannot be empty")]
+        public string LastName { get; set; }
+    }
+}
