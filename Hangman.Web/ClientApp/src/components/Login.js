8be87f5b6e5679497167b7e2ecb745bee606b2b3
bsumitiw <bsumitiw@gmail.com>
2018-09-09 08:51:59 +0545

    Added Register and Login js

---
+++ Hangman.Web/ClientApp/src/components/Login.js
@@ -0,0 +1,36 @@
+﻿import React, { Component } from 'react';
+import './Login.css';
+import { Container, Row, Col, Input, Button, Fa, Card, CardBody, ModalFooter } from 'mdbreact';
+import { Link } from 'react-router-dom';
+
+export class Login extends Component {
+    render() {
+        return (
+            <Container>
+                <Row className="flex-center">
+                    <Col md="6">
+                        <Card>
+                            <CardBody>
+                                <form className="login-form">
+                                    <p className="h4 text-center py-4">Log In</p>
+                                    <div className="grey-text">
+                                        <Input label="Username" icon="user" group type="text" validate error="wrong" success="right" />
+                                        <Input label="Password" icon="lock" group type="password" validate />
+                                    </div>
+                                    <div className="text-center py-4 mt-3">
+                                        <Button color="cyan" type="button">Login</Button>
+                                    </div>
+                                </form>
+                            </CardBody>
+                            <ModalFooter className="mx-5 pt-3 mb-1">
+                                <Link to="/register">
+                                    <p className="font-small grey-text d-flex justify-content-end">Not a member? Sign Up</p>
+                                </Link>
+                            </ModalFooter>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
