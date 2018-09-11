35ca288bc755d48e036dc43b860010d9c20cdb97
bsumitiw <bsumitiw@gmail.com>
2018-09-11 07:58:07 +0545

    Added missing module

---
+++ Hangman.Web/ClientApp/src/components/UserProfile.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+import { UserLayout } from './UserLayout';
+
+export class UserProfile extends Component {
+
+    render() {
+        return (
+            <UserLayout>
+                <div>User Profile</div>
+            </UserLayout>
+        );
+    }
+}
---
+++ Hangman.Web/ClientApp/src/components/Userdashboard.js
@@ -0,0 +1,37 @@
+﻿import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Glyphicon, Nav, Navbar, NavItem, Col, Grid, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import './Admindashboard.css';
+
+export class Userdashboard extends Component {
+    displayName = Userdashboard.name
+
+    render() {
+        return (
+            <Navbar inverse fixedTop fluid collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to={'/applyleave'}>User Dashboard</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <LinkContainer to={'/applyleave'}>
+                            <NavItem>
+                                <Glyphicon glyph='th-list' /> Apply Leave
+              </NavItem>
+                        </LinkContainer>
+                        <LinkContainer to={'/login'} exact>
+                            <NavItem>
+                                <Glyphicon glyph='user' /> Logout
+                              
+              </NavItem>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+}
